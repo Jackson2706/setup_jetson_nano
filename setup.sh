644fed7b35bcf03f9892938c8de46ee4627a38a7
Jackson2706 <dung27062002@gmail.com>
2024-04-11 09:05:39 +0700

    ssh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,6 @@ git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision
 cd torchvision
 sudo python3 setup.py install
 
+wget https://nvidia.box.com/shared/static/jy7nqva7l88mq9i8bw3g3sklzf4kccn2.whl -O onnxruntime_gpu-1.16.0-cp38-cp38-linux_aarch64.whl
+pip3 install onnxruntime_gpu-1.16.0-cp38-cp38-linux_aarch64.whl
+
